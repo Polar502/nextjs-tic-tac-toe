613a7c787b382db3b6a4cfd780455fb7939b3bb5
Polar502 <marioarita502@gmail.com>
2022-09-22 14:52:38 -0600

    add @apply tailwind css in globals.css

--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,18 @@
 // import Image from "next/image";
 // import styles from "../styles/Home.module.css";
 
-
 // Importame la funcion "Tablero" desde el archivo "tablero" en esta misma carpeta
 import Tablero from "./tablero";
 
 const Totito = () => {
   return (
-    <div className="flex items-center bg-emerald-500 min-h-screen">
-      {/* invocar funcion Tablero */}
-      <Tablero className="max-w-4xl" />
+    <div className="flex flex-col bg-sky-600 items-center justify-center h-screen">
+      <div className="">
+        {/* No necesario (quitar) */}
+        <h1 className="h1-text">Your turn Player X || O </h1>
+        {/* invocar funcion Tablero */}
+        <Tablero />
+      </div>
     </div>
   );
 };
--- pages/tablero.js
+++ pages/tablero.js
@@ -9,42 +9,23 @@
 
 const Tablero = () => {
   return (
-    <div className="grid grid-cols-3 gap-8 mx-auto p-10">
-      {/* Temporalmente div */}
+    <div className="grid grid-cols-3 gap-4 p-8 bg-sky-700">
       {/* Tailwind style: contenido centrado, fuente con estilo semibol color blanco, borde blanco con un groso de 2px, fondo color esmeralda 400, efecto de sombra y opacidad de duración 300 milisegundos*/}
-      
+
       {/* Primera Fila -> Secciones 1, 2, 3 */}
-      <div className="flex items-center justify-center font-semibold text-white h-24 w-24 border-white border-2 bg-emerald-400 rounded-xl shadow-xl hover:bg-opacity-25 duration-300">
-        1
-      </div>
-      <div className="flex items-center justify-center font-semibold text-white h-24 w-24 border-white border-2 bg-emerald-400 rounded-xl shadow-xl hover:bg-opacity-25 duration-300">
-        2
-      </div>
-      <div className="flex items-center justify-center font-semibold text-white h-24 w-24 border-white border-2 bg-emerald-400 rounded-xl shadow-xl hover:bg-opacity-25 duration-300">
-        3
-      </div>
+      <div className="div-totito">1</div>
+      <div className="div-totito">2</div>
+      <div className="div-totito">3</div>
 
       {/* Segunda Fila -> Secciones 4, 5, 6 */}
-      <div className="flex items-center justify-center font-semibold text-white h-24 w-24 border-white border-2 bg-emerald-400 rounded-xl shadow-xl hover:bg-opacity-25 duration-300">
-        4
-      </div>
-      <div className="flex items-center justify-center font-semibold text-white h-24 w-24 border-white border-2 bg-emerald-400 rounded-xl shadow-xl hover:bg-opacity-25 duration-300">
-        5
-      </div>
-      <div className="flex items-center justify-center font-semibold text-white h-24 w-24 border-white border-2 bg-emerald-400 rounded-xl shadow-xl hover:bg-opacity-25 duration-300">
-        6
-      </div>
+      <div className="div-totito">4</div>
+      <div className="div-totito">5</div>
+      <div className="div-totito">6</div>
 
       {/* Tercera Fila -> Secciones 7, 8, 9 */}
-      <div className="flex items-center justify-center font-semibold text-white h-24 w-24 border-white border-2 bg-emerald-400 rounded-xl shadow-xl hover:bg-opacity-25 duration-300">
-        7
-      </div>
-      <div className="flex items-center justify-center font-semibold text-white h-24 w-24 border-white border-2 bg-emerald-400 rounded-xl shadow-xl hover:bg-opacity-25 duration-300">
-        8
-      </div>
-      <div className="flex items-center justify-center font-semibold text-white h-24 w-24 border-white border-2 bg-emerald-400 rounded-xl shadow-xl hover:bg-opacity-25 duration-300">
-        9
-      </div>
+      <div className="div-totito">7</div>
+      <div className="div-totito">8</div>
+      <div className="div-totito">9</div>
     </div>
   );
 };
