4fbef568241c50417832249c62b83d6faa31d499
Polar502 <marioarita502@gmail.com>
2022-09-25 19:13:53 -0600

    Add in Readme.dm demo view on Heroku

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import Tablero from "./components/tablero";
 function MyTicTacToe() {
   return (
     <div className="flex flex-col bg-neutral-900 items-center justify-center h-screen">
+      {/* Llamado del tablero */}
       <Tablero/>
   </div>
 
